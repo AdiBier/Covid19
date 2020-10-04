9be534b7c1f93d9c43d7714dda3eeb50db024eaa
Adrian Bierkat <s9664ab@ms.wwsi.edu.pl>
2020-10-04 20:20:19 +0200

    Done

---
+++ Covid19/Controllers/PatientsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Covid19.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Covid19.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly DpDataContext _context;
+
+        public PatientsController(DpDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Patients.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult RegisterPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+
+            return Created("/api/patients/", patient);
+        }
+    }
+}
---
+++ Covid19/Model/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfThePositiveTest { get; set; }
+    }
+}
